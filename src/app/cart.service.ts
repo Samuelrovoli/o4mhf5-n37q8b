6d9d5ebc9b585cff9718b74ba986d3db909a7cce
Samuelrovoli <128751847+Samuelrovoli@users.noreply.github.com>
2023-06-20 19:26:33 -0300

    4 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 import { Injectable } from '@angular/core';
 
@@ -6,8 +7,16 @@ import { Injectable } from '@angular/core';
 })
 export class CartService {
 
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
+  
   /*propriedade para armazenar a matriz dos produtos atuais no carrinho.*/
   items: Product[] = [];
+
+ constructor(
+    private http: HttpClient
+  ) {}
 
   /* método anexa um produto a uma matriz de items */
   addToCart(product: Product) {
